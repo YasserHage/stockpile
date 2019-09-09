9694ec3a627bb8343089692a6ae74ac7de913c2b
Yasser El Hage <yhage@betait.com.br>
2019-09-09 16:39:16 -0300

    First commit

---
+++ src/main/java/com/stockpile/main/StockpileApplication.java
@@ -0,0 +1,13 @@
+package com.stockpile.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockpileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockpileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockpile/main/StockpileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockpile.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockpileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
